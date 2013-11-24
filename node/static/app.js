3798411c364e3de6e916d109f61aaeff64b8eb4b
Anting Shen <antingshen@berkeley.edu>
2013-11-24 09:54:44 -0800

    more fixes

--- node/static/app.js
+++ node/static/app.js
@@ -15,8 +15,8 @@ function($scope,$timeout){
 	}
 
 	GAME_HEIGHT = 600;
-	GAME_WIDTH = 680;
-	WORD_HEIGHT = 50;
+	GAME_WIDTH = window.innerWidth;
+	WORD_HEIGHT = 34;
 	WORD_WIDTH = 100;
 
 	$scope.words = [];
@@ -34,12 +34,14 @@ function($scope,$timeout){
 		player.score = 0;
 		$scope.player = player;
 		player.awardWord = awardWord.bind(player);
+		return player;
 	}
     $scope.socket = io.connect('/');
     $scope.socket.on('onconnected', function (data) {
         console.log('Connected. ID = ' + data.id);
-        initPlayer(data.id);
+        $scope.player = initPlayer(data.id);
     });
+    $scope.opponent = initPlayer(1);
 	inputBox = document.getElementById('inputBox');
 	$scope.bodyClick = function(){
 		inputBox.focus();
@@ -85,7 +87,7 @@ function($scope,$timeout){
 				return;
 			}
             // For test purposes
-			this.life -= 0;
+			this.life -= 2;
 			this.Yoffset = (1-this.life/this.maxLife)*(GAME_HEIGHT+WORD_HEIGHT)-WORD_HEIGHT;
 			this.wordTick();
 		}.bind(this), 500);
@@ -153,12 +155,31 @@ function($scope,$timeout){
 		typeLetter.bind(word)(obj.letter, 1);
 	}
 
+	$scope.timeRemaining = 90;
+	$scope.countdownStarted = false;
+	$scope.timerTick = function(){
+		if ($scope.timeRemaining <= 0){
+			while ($scope.words.length){
+				$scope.words[0].destroy();
+			}
+			return;
+		}
+		$timeout(function(){
+			$scope.timeRemaining -= 1;
+			$scope.timerTick();
+		},1000);
+	}
+	$scope.startTimer = function(){
+		$scope.countdownStarted = true;
+		$scope.socket.emit('startGame', {pid:$scope.player.pid});
+		$scope.timerTick();
+	}
+
 	$scope.socket.on('newWord', $scope.createWord);
 	// param: data = {word: word object}
     // can change this later
 
 	$scope.socket.on('opponentKey', $scope.opponentKey);
-
 	// param: {word: String, letter: String}
 })
 
